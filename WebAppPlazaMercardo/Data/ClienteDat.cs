76a03165bc61e2ae16acaa3987934ba09d35d9b5
jeanp00 <semijeanp@gmail.com>
2024-11-02 09:45:44 -0500

    Se resuelven algunos conflictos en la capa data

--- WebAppPlazaMercardo/Data/ClienteDat.cs
+++ WebAppPlazaMercardo/Data/ClienteDat.cs
@@ -43,23 +43,20 @@ namespace Data
         }
 
 
-        //Metodo para guardar un nuevo cliente
-        public bool saveClient(string c_names,string c_lastnames,
-            string c_mail,string c_phone,string c_addres)
+        public bool saveClient(string c_names, string c_lastnames, string c_mail, string c_phone, string c_addres)
         {
             bool executed = false;
             int row;
 
             MySqlCommand objSelectCmd = new MySqlCommand();
             objSelectCmd.Connection = objPer.openConnection();
-            objSelectCmd.CommandText = "spInsertCategory"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandText = "spInsertClient"; // Nombre del procedimiento almacenado
             objSelectCmd.CommandType = CommandType.StoredProcedure;
-            objSelectCmd.Parameters.Add("cli_nombres", MySqlDbType.VarString).Value = c_names;
-            objSelectCmd.Parameters.Add("cli_apellidos", MySqlDbType.VarString).Value = c_lastnames;
-            objSelectCmd.Parameters.Add("cli_correo", MySqlDbType.VarString).Value = c_mail;
-            objSelectCmd.Parameters.Add("cli_telefono", MySqlDbType.VarString).Value = c_phone;
-            objSelectCmd.Parameters.Add("cli_direccion", MySqlDbType.VarString).Value = c_addres;
-
+            objSelectCmd.Parameters.Add("c_names", MySqlDbType.VarString).Value = c_names;
+            objSelectCmd.Parameters.Add("c_lastnames", MySqlDbType.VarString).Value = c_lastnames;
+            objSelectCmd.Parameters.Add("c_mail", MySqlDbType.VarString).Value = c_mail;
+            objSelectCmd.Parameters.Add("c_phone", MySqlDbType.VarString).Value = c_phone;
+            objSelectCmd.Parameters.Add("c_addres", MySqlDbType.VarString).Value = c_addres;
 
             try
             {
@@ -75,8 +72,8 @@ namespace Data
             }
             objPer.closeConnection();
             return executed;
-
         }
+
 
         //Metodo para actualizar un cliente
         public bool updateClient(int c_id, string c_names, string c_lastnames,
