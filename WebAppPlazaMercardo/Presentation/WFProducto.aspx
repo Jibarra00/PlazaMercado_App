bffa746f971b92024a5b137ce91f5ab4607d3e4f
marcelalizcano <linalizcanogaviria@gmail.com>
2024-11-06 12:18:57 -0500

    Se sube APDM de producto y modificación de la capa data

--- WebAppPlazaMercardo/Presentation/WFProducto.aspx
+++ WebAppPlazaMercardo/Presentation/WFProducto.aspx
@@ -2,4 +2,42 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+    <br />
+    <%--codigo--%>
+    <asp:Label ID="Label1" runat="server" Text="Ingrese el codigo"></asp:Label>
+    <asp:TextBox ID="TBCodigo" runat="server"></asp:TextBox>
+    <br />
+    <%--Descripcion--%>
+    <asp:Label ID="Label2" runat="server" Text="Ingrese la descripción"></asp:Label>
+    <asp:TextBox ID="TBDescripcion" runat="server"></asp:TextBox>
+    <br />
+    <%--Catidad--%>
+    <asp:Label ID="Label3" runat="server" Text="Ingrese la cantidad"></asp:Label>
+    <asp:TextBox ID="TBCantidad" runat="server"></asp:TextBox>
+    <br />
+    <%--Precio--%>
+    <asp:Label ID="Label4" runat="server" Text="Ingrese el precio"></asp:Label>
+    <asp:TextBox ID="TBPrecio" runat="server"></asp:TextBox>
+    <br />
+    <%--Proveedor--%>
+    <asp:Label ID="Label5" runat="server" Text="Seleccione el proveedor"></asp:Label>
+    <asp:DropDownList ID="DDLProveedor" runat="server"></asp:DropDownList>
+    <br />
+    <%--Categorias--%>
+    <asp:Label ID="Label6" runat="server" Text="Seleccione la categoria"></asp:Label>
+    <asp:DropDownList ID="DDLCategoria" runat="server"></asp:DropDownList>
+    <br />
+    <%--Botones guardar y actualizar--%>
+    <div>
+        <asp:Button ID="BTnSave" runat="server" Text="Guardar" OnClick="BTnSave_Click" />
+        <asp:Button ID="BTnUpdate" runat="server" Text="Actializar" />
+        <asp:Label ID="LblMsg" runat="server" Text=""></asp:Label>
+    </div>
+    <br />
+    <%--Lista de productos--%>
+    <div>
+        <asp:GridView ID="GVProductos" runat="server"></asp:GridView>
+    </div>
+
+
 </asp:Content>
--- WebAppPlazaMercardo/Presentation/WFProducto.aspx.cs
+++ WebAppPlazaMercardo/Presentation/WFProducto.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,70 @@ namespace Presentation
 {
     public partial class WFProducto : System.Web.UI.Page
     {
+        //Crea los objetos
+        ProductoLog objProd = new ProductoLog();
+        ProveedorLog objPro = new ProveedorLog();
+        CategoriaLog objCat = new CategoriaLog();
+
+        private int _id, _quantity, _fkCategory, _fkProvider;
+        private string _code, _description;
+        private double _price;
+        private bool executed = false;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                
+                showProductos();
+                showCategoriaDDL();
+                showProvedoresDDL();
+            }
+        }
+        private void showProductos()
+        {
+            DataSet ds = new DataSet();
+            ds = objProd.showProducts();
+            GVProductos.DataSource = ds;
+            GVProductos.DataBind();
+        }
+        private void showProvedoresDDL()
+        {
+            DDLProveedor.DataSource = objPro.showProveedorDDL();
+            DDLProveedor.DataValueField = "prov_id";
+            DDLProveedor.DataTextField = "prov_nombre";
+            DDLProveedor.DataBind();
+            DDLProveedor.Items.Insert(0, "Seleccione el proveedor");
+        }
+        private void showCategoriaDDL()
+        {
+            DDLCategoria.DataSource = objCat.showCategoriesDDL();
+            DDLCategoria.DataValueField = "cat_id";
+            DDLCategoria.DataTextField = "cat_descripcion";
+            DDLCategoria.DataBind();
+            DDLCategoria.Items.Insert(0, "Seleccione la categoria");
+        }
 
+        protected void BTnSave_Click(object sender, EventArgs e)
+        {
+            _code = TBCodigo.Text;
+            _description = TBDescripcion.Text;
+            _quantity = Convert.ToInt32(TBCantidad.Text);
+            _price = Convert.ToDouble(TBPrecio.Text);
+            _fkCategory = Convert.ToInt32(DDLCategoria.Text);
+            _fkProvider = Convert.ToInt32(DDLProveedor.Text);
+
+
+            executed = objProd.saveProducts(_code, _description, _quantity, _price, _fkCategory, _fkProvider);
+
+            if (executed)
+            {
+                LblMsg.Text = "Se guardo el producto";
+                showProductos();
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar";
+            }
         }
     }
 }
