4bbec24af21e8c37f2697f8c9d73c662af444df0
marcelalizcano <linalizcanogaviria@gmail.com>
2024-11-06 13:20:24 -0500

    se sube APDM de rol

--- WebAppPlazaMercardo/Presentation/WFRol.aspx.cs
+++ WebAppPlazaMercardo/Presentation/WFRol.aspx.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Logic;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,9 +11,65 @@ namespace Presentation
 {
     public partial class WFRol : System.Web.UI.Page
     {
+        //crear los objetos
+        RolLog objRol = new RolLog();
+
+        private int _id;
+        private string _nombre, _descripcion;
+        private bool executed = false;
+
+
+
+
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                //aqui se invocan todos los metodos
+                showRol();
+            }
+                
 
         }
+
+        
+
+        //metodo para los roles
+        private void showRol()
+        {
+            DataSet ds = new DataSet();
+            ds = objRol.showRol();
+            GVRol.DataSource = ds;
+            GVRol.DataBind();
+
+        }
+        //eventos que se ejecutan cuando se da clic en los botones
+
+        protected void BtnSave_Click(object sender, EventArgs e)
+        {
+            _nombre = TBNombre.Text;
+            _descripcion = TBdescripcion.Text;
+
+            executed = objRol.saveRol(_nombre, _descripcion);
+
+            if (executed)
+            {
+                LblMsg.Text = "el rol se guardo exitosamente";
+                showRol();
+
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar";
+            }
+
+
+            
+
+
+        
+        }
+
     }
 }
