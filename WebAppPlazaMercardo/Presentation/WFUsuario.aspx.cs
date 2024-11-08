5e06aab0547963853223aac219719b2619c8765d
Deybison Perez <deybison2026@gmail.com>
2024-11-08 12:41:50 -0500

    Se sube parte del codigo(corregir)

--- WebAppPlazaMercardo/Presentation/WFUsuario.aspx.cs
+++ WebAppPlazaMercardo/Presentation/WFUsuario.aspx.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Web;
+using System.Web.Services;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -26,19 +27,60 @@ namespace Presentation
         {
             if (!IsPostBack)
             {
-                showUsuario();
+                //showUsuario();
                 showClienteDDL();
                 showEmpleadoDDL();
                 showRolDDL();
             }
             
         }
-        private void showUsuario()
+        /*
+         * Atributo [WebMethod] en ASP.NET, permite que el método sea expuesto como 
+         * parte de un servicio web, lo que significa que puede ser invocado de manera
+         * remota a través de HTTP.
+         */
+        [WebMethod]
+        public static object ListUsuario()
         {
-            DataSet ds = new DataSet();
-            ds = objUsu.showUsuario();
-            GVUsuarios.DataSource = ds;
-            GVUsuarios.DataBind();
+            UsuarioLog objUsu = new UsuarioLog();
+
+            // Se obtiene un DataSet que contiene la lista de los usuarios desde la base de datos.
+            var dataSet = objUsu.showUsuario();
+            // Se crea una lista para almacenar los usuarios que se van a devolver.
+            var usuarioList = new List<object>();
+            // Se itera sobre cada fila del DataSet (que representa un usuario).
+            foreach(DataRow row in dataSet.Tables[0].Rows)
+            {
+                usuarioList.Add(new
+                {
+                    UsuarioID = row["usu_id"],
+                    Correo = row["usu_contrasena"],
+                    Salt = row["usu_salt"],
+                    Estado = row["usu_estado"],
+                    Fecha_creacion = row["usu_fecha_creacion"],
+                    FkRol = row["tbl_rol_rol_id"],
+                    nameRol = row["rol_nombre"],
+                    FkEmpleado = row["tbl_empleado_emp_id"],
+                    nameEmpleado = row["emp_nombres"],
+                    FkCliente = row["tbl_cliente_cli_id"],
+                    nameCliente = row["cli_nombres"]
+                });
+            }
+            // Devuelve un objeto en formato JSON que contiene la lista de usuarios.
+            return new {data = usuarioList};
+        }
+
+        
+
+        //metodo para eliminar un usuario por su id
+        [WebMethod]
+        public static bool DeleteUsuario(int id)
+        {
+            // Crear una instancia de la clase de lógica de un usuario
+            UsuarioLog objUsu = new UsuarioLog();
+            // invocar al metodo para eliminar un usuario y devolver el resultado
+            return objUsu.deleteUsuario(id);
+
         }
         private void showClienteDDL()
         {
@@ -46,7 +88,7 @@ namespace Presentation
             DDLCliente.DataValueField = "cli_id";
             DDLCliente.DataTextField = "cli_nombres";
             DDLCliente.DataBind();
-            DDLCliente.Items.Insert(0, "Seleccione");
+            DDLCliente.Items.Insert(0, "Seleccione cliente");
         }
         private void showRolDDL()
         {
@@ -64,6 +106,22 @@ namespace Presentation
             DDLEmpleado.DataBind();
             DDLEmpleado.Items.Insert(0, "Seleccione empleado");
         }
+
+        //Metodo para limpiar los Texbox y los DDL
+        private void clear()
+        {
+            HFUsuarioID.Value = "";
+            TBEmail.Text = "";
+            TBPassword.Text = "";
+            TBSalt.Text = "";
+            TBState.Text = "";
+            TBDate.Text = "";
+            DDLRol.SelectedIndex = 0;
+            DDLEmpleado.SelectedIndex = 0;
+            DDLCliente.SelectedIndex = 0;
+            
+        }
+
 
         //Eventos cuando se ejecutan al dar click en los botones
         protected void BtnSave_Click(object sender, EventArgs e)
@@ -84,7 +142,7 @@ namespace Presentation
                 if (executed)
                 {
                     LblMsg.Text = "El usuario se creo correctamente";
-                    showUsuario();
+                    clear();
                 }
                 else
                 {
@@ -93,5 +151,38 @@ namespace Presentation
             
             
         }
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            // Verifica si se ha seleccionado un usuario para actualizar
+            if (string.IsNullOrEmpty(HFUsuarioID.Value))
+            {
+                LblMsg.Text = "No se ha seleccionado un usuario para actualizar";
+                return;
+            }
+            _id = Convert.ToInt32(HFUsuarioID.Value);
+            _email = TBEmail.Text;
+            _password = TBPassword.Text;
+            _salt = TBSalt.Text;
+            _state = TBSalt.Text;
+            _Create_Date = Convert.ToDateTime(TBDate.Text);
+            _fkrol = Convert.ToInt32(DDLRol.SelectedValue);
+            _fkempleado = Convert.ToInt32(DDLEmpleado.SelectedValue);
+            _fkcliente = Convert.ToInt32(DDLCliente.SelectedValue);
+
+            executed = objUsu.updateUsuario(_id,_email,_password,_salt,_state,_Create_Date,_fkrol,_fkempleado,_fkcliente);
+
+            if (executed)
+            {
+                LblMsg.Text = "El usuario se actualizo correctamente";
+                clear();
+            }
+            else
+            {
+                LblMsg.Text = "Error al actualizar";
+            }
+        }
+
+
     }
 }
--- WebAppPlazaMercardo/Presentation/WFUsuario.aspx.designer.cs
+++ WebAppPlazaMercardo/Presentation/WFUsuario.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Presentation
     {
 
         /// <summary>
+        /// Control HFUsuarioID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFUsuarioID;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
@@ -184,14 +193,5 @@ namespace Presentation
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LblMsg;
-
-        /// <summary>
-        /// Control GVUsuarios.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GVUsuarios;
     }
 }
