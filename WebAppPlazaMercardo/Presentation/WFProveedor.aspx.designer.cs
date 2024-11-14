ff290299c2e86c855184c175dc48ef8ee09dffd7
marcelalizcano <linalizcanogaviria@gmail.com>
2024-11-14 16:56:57 -0500

    se sube APDM de provedor y se modifica en la capa data proveedor

--- WebAppPlazaMercardo/Presentation/WFProveedor.aspx.designer.cs
+++ WebAppPlazaMercardo/Presentation/WFProveedor.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Presentation
     {
 
         /// <summary>
+        /// Control HFProveedorID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFProveedorID;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
@@ -76,14 +85,5 @@ namespace Presentation
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LblMsg;
-
-        /// <summary>
-        /// Control GVProveedor.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GVProveedor;
     }
 }
