b09d5362bf198fd2b4a6fa7b604bc622398e846c
marcelalizcano <linalizcanogaviria@gmail.com>
2024-11-14 17:48:48 -0500

    se sube APDM de rol

--- WebAppPlazaMercardo/Presentation/WFRol.aspx.designer.cs
+++ WebAppPlazaMercardo/Presentation/WFRol.aspx.designer.cs
@@ -15,6 +15,15 @@ namespace Presentation
     {
 
         /// <summary>
+        /// Control HFRolID.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HiddenField HFRolID;
+
+        /// <summary>
         /// Control Label1.
         /// </summary>
         /// <remarks>
@@ -76,14 +85,5 @@ namespace Presentation
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label LblMsg;
-
-        /// <summary>
-        /// Control GVRol.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView GVRol;
     }
 }
