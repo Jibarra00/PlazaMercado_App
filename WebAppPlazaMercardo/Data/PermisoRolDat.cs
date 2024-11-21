2a37ab106486593b8d589fb15170ace412fc958a
Deybison Perez <deybison2026@gmail.com>
2024-11-21 14:56:36 -0500

    Se hizo crud completo de permiso rol y se sube DDL de permiso

---
+++ WebAppPlazaMercardo/Data/PermisoRolDat.cs
@@ -0,0 +1,115 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace Data
+{
+    public class PermisoRolDat
+    {
+        Persistence objPer = new Persistence();
+
+        public DataSet showPermisoRol()
+        {
+            MySqlDataAdapter objAdapter = new MySqlDataAdapter();
+            DataSet objData = new DataSet();
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spSelectPermisoRol";
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objAdapter.SelectCommand = objSelectCmd;
+            objAdapter.Fill(objData);
+            objPer.closeConnection();
+            return objData;
+        }
+
+        public bool savePermisoRol(int _fkPermiso, int _fkRol,DateTime _date)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spInsertPermisoRol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_permiso_id", MySqlDbType.Int32).Value = _fkPermiso;
+            objSelectCmd.Parameters.Add("p_rol_id", MySqlDbType.Int32).Value = _fkRol;
+            objSelectCmd.Parameters.Add("p_date", MySqlDbType.DateTime).Value = _date;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool updatePermisoRol(int _id, int _fkPermiso, int _fkRol, DateTime _date) 
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spUpdatePermisoRol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_rol_permiso", MySqlDbType.Int32).Value = _id;
+            objSelectCmd.Parameters.Add("p_fkpermiso", MySqlDbType.Int32).Value = _fkPermiso;
+            objSelectCmd.Parameters.Add("p_fkrol", MySqlDbType.Int32).Value = _fkRol;
+            objSelectCmd.Parameters.Add("p_date", MySqlDbType.DateTime).Value = _date;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+
+        public bool deletePermisoRol(int _idPermisoRol)
+        {
+            bool executed = false;
+            int row;
+
+            MySqlCommand objSelectCmd = new MySqlCommand();
+            objSelectCmd.Connection = objPer.openConnection();
+            objSelectCmd.CommandText = "spDeletePermisoRol"; //nombre del procedimiento almacenado
+            objSelectCmd.CommandType = CommandType.StoredProcedure;
+            objSelectCmd.Parameters.Add("p_id", MySqlDbType.Int32).Value = _idPermisoRol;
+
+            try
+            {
+                row = objSelectCmd.ExecuteNonQuery();
+                if (row == 1)
+                {
+                    executed = true;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error " + e.ToString());
+            }
+            objPer.closeConnection();
+            return executed;
+        }
+    }
+}
---
+++ WebAppPlazaMercardo/Presentation/WFPermisoRol.aspx.cs
@@ -0,0 +1,143 @@
+﻿using Logic;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Presentation
+{
+    public partial class WFPermisoRol : System.Web.UI.Page
+    {
+        PermisoLog objPer = new PermisoLog();
+        RolLog objRol = new RolLog();
+        PermisoRolLog objPerRol = new PermisoRolLog();
+
+        private int _id, _fkRol, _fkPermiso;
+        private DateTime _date;
+        private bool executed = false;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack) 
+            {
+                TBFecha.Text = DateTime.Now.ToString("yyyy-MM-dd");
+
+                showPermisoDDL();
+                showRolDDL();
+            }
+        }
+
+        /*
+         * Atributo [WebMethod] en ASP.NET, permite que el método sea expuesto como 
+         * parte de un servicio web, lo que significa que puede ser invocado de manera
+         * remota a través de HTTP.
+         */
+        [WebMethod]
+        public static object ListPermisoRol()
+        {
+            PermisoRolLog objPerRol = new PermisoRolLog();
+
+            var dataSet = objPerRol.showPermisoRol();
+
+            var permisosRolList = new List<object>();
+
+            foreach (DataRow row in dataSet.Tables[0].Rows)
+            {
+                permisosRolList.Add(new
+                {
+                    PermisoRolID = row["rol_permiso"],
+                    FkPermiso = row["tbl_permiso_per_id"],
+                    namePermiso = row["per_nombre"],
+                    FkRol = row["tbl_rol_rol_id"],
+                    nameRol = row["rol_nombre"],
+                    Date = Convert.ToDateTime(row["per_rol_fecha_asignacion"]).ToString("yyyy-MM-dd ddd")
+                });
+            }
+            return new { data = permisosRolList };
+        }
+        [WebMethod]
+
+        public static bool DeletePermisosRol(int id)
+        {
+            PermisoRolLog objPerRol = new PermisoRolLog();
+
+            return objPerRol.deletePermisoRol(id);
+        }
+
+        private void showPermisoDDL()
+        {
+            DDLPermisos.DataSource = objPer.showPermisoDDL();
+            DDLPermisos.DataValueField = "per_id";
+            DDLPermisos.DataTextField = "per_nombre";
+            DDLPermisos.DataBind();
+            DDLPermisos.Items.Insert(0, "Seleccione el Rol");
+        }
+
+
+        private void showRolDDL()
+        {
+            DDLRoles.DataSource = objRol.showrolDDL();
+            DDLRoles.DataValueField = "rol_id";
+            DDLRoles.DataTextField = "rol_nombre";
+            DDLRoles.DataBind();
+            DDLRoles.Items.Insert(0, "Seleccione el Rol");
+        }
+
+        private void clear()
+        {
+            HFPermisoRolID.Value = "";
+            DDLPermisos.SelectedIndex = 0;
+            DDLRoles.SelectedIndex = 0;
+            TBFecha.Text = DateTime.Now.ToString("yyyy-MM-dd");
+        }
+
+        protected void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            _fkPermiso = Convert.ToInt32(DDLPermisos.SelectedValue);
+            _fkRol = Convert.ToInt32(DDLRoles.SelectedValue);
+            _date = DateTime.Parse(TBFecha.Text);
+
+            executed = objPerRol.savePermisoRol(_fkPermiso, _fkRol,_date);
+
+            if (executed)
+            {
+                LblMsg.Text = "El permiso rol se guardo exitosamente!";
+                clear();//Se invoca el metodo para limpiar los campos 
+            }
+            else
+            {
+                LblMsg.Text = "Error al guardar";
+            }
+        }
+
+
+        protected void BtnUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(HFPermisoRolID.Value))
+            {
+                LblMsg.Text = "No se ha seleccionado un permiso rol para actualizar";
+                return;
+            }
+
+            _id = Convert.ToInt32(HFPermisoRolID.Value);
+            _fkPermiso = Convert.ToInt32(DDLPermisos.SelectedValue);
+            _fkRol = Convert.ToInt32(DDLRoles.SelectedValue);
+            _date = DateTime.Parse(TBFecha.Text);
+
+            executed = objPerRol.updatePermisoRol(_id, _fkPermiso, _fkRol, _date);
+
+            if (executed)
+            {
+                LblMsg.Text = "El permiso rol se actualizo exitosamente!";
+                clear();//Se invoca el metodo para limpiar los campos 
+            }
+            else
+            {
+                LblMsg.Text = "Error al actualizo";
+            }
+        }
+    }
+}
